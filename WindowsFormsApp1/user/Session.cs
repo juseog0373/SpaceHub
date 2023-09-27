9c343f5d7375c66eb822565d5c849f1db40e4fbe
moonch030 <cogus2010@naver.com>
2023-09-27 17:50:18 +0900

    유저 id session 생성

---
+++ WindowsFormsApp1/user/Session.cs
@@ -0,0 +1,28 @@
+﻿using Org.BouncyCastle.Bcpg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace userIdSession
+{
+    internal class Session
+    {
+
+        private string userId = null;
+
+
+        public Session(string userId)
+        {
+
+            this.userId = userId;
+        }
+
+        public string getUserId() { return userId; }
+        public void setUserId(string s)
+        {
+            userId = s;
+        }
+    }
+}
--- WindowsFormsApp1/user/userLoginForm.cs
+++ WindowsFormsApp1/user/userLoginForm.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 using dbConnectSpace;
 using MySql.Data.MySqlClient; // SQL 연동을 위한 using
 using static dbConnectSpace.dbConnection; //dbConnection 임포트
-
+using userIdSession;
 
 namespace WindowsFormsApp1
 {
@@ -60,6 +60,8 @@ namespace WindowsFormsApp1
                 }
                 if (loginStatus == 1)
                 {
+
+                    Session userIdSession = new Session(userId);
                     MessageBox.Show(mdr.GetString("userName")+"님 환영합니다.");
 
                     selectClassForm selectClassForm = new selectClassForm();
